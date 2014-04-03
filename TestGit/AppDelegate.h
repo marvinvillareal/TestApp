5c0248c4d3763e3b81e9a0fe99d1bcba48b19d9e
Anwar Julian T. Bocar <ajbocar@gmail.com>
2014-04-03 14:21:20 +0800

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Andy Schachtel on 4/3/14.
+//  Copyright (c) 2014 Sourcefit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Andy Schachtel on 4/3/14.
+//  Copyright (c) 2014 Sourcefit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
